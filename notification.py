c1433d89f74dfcd3ff937092f1b75e553a529c41
bdeon <bdeon@student.21-school.ru>
2022-11-15 23:10:36 +0700

    try-expect construction done

--- notification.py
+++ notification.py
@@ -1,64 +1,40 @@
 import requests
 import time
 import hashlib
+
+import selenium.common
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
 url = 'https://edu.21-school.ru'
 CodeReview = 'https://edu.21-school.ru/projects/code-review'
 
-driver = webdriver.Chrome('C:\\chromedriver\chromedriver.exe')
-time.sleep(2)
-driver.get(url)
-time.sleep(5)
-login = driver.find_element(By.NAME, "username")
-password = driver.find_element(By.NAME, "password")
-login.send_keys("name")
-password.send_keys("pass")
-driver.find_element(By.XPATH, '//*[@id="login"]/div/div/div[2]/div/div/form/div[3]/button').click()
 
-time.sleep(100)
-driver.quit()
+def WebMonitoring(MyName, MyPass):
+    try:
+        driver = webdriver.Chrome('C:\\chromedriver\chromedriver.exe')
+        time.sleep(2)
+        driver.get(url)
+        time.sleep(5)
+        login = driver.find_element(By.NAME, "username")
+        password = driver.find_element(By.NAME, "password")
+        login.send_keys(MyName)
+        password.send_keys(MyPass)
+        driver.find_element(By.XPATH, '//*[@id="login"]/div/div/div[2]/div/div/form/div[3]/button').click()
+        time.sleep(5)
+        driver.get(CodeReview)
+        time.sleep(5)
+        elem = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/div/section/div/section[2]')
+        # цикл для проверки нахождения элемента
+
+        time.sleep(100)
+        driver.quit()
+    except selenium.common.NoSuchElementException:
+        WebMonitoring()
 
 
-# response = urlopen(url).read()
-# encode = 'utf-8'
-# dec = response.decode(encode)
-# f = open("pages.txt", "w")
-# f.write(dec)
-# f.close()
+if __name__ == "__main__":
+    WebMonitoring(MyName, MyPass)
 
-# currentHash = hashlib.sha224(response).hexdigest()
-# print("running")
-# time.sleep(10)
-# while True:
-#     try:
-#         response = urlopen(url).read()
-#
-#         # create a hash
-#         currentHash = hashlib.sha224(response).hexdigest()
-#
-#         # wait for 30 seconds
-#         time.sleep(30)
-#
-#         # perform the get request
-#         response = urlopen(url).read()
-#
-#         # create a new hash
-#         newHash = hashlib.sha224(response).hexdigest()
-#
-#         # check if new hash is same as the previous hash
-#         if newHash == currentHash:
-#             continue
-#         else:
-#             print("something changed")
-#             response = urlopen(url).read()
-#
-#     # create a hash
-#             currentHash = hashlib.sha224(response).hexdigest()
-#
-#     # wait for 30 seconds
-#             time.sleep(30)
-#             continue
-#     except Exception as e:
-#         print("error")
+
