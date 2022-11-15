51cbe2929850634b173827cffc93fda5b69e4fa9
bdeon <bdeon@student.21-school.ru>
2022-11-16 00:06:33 +0700

    refresh cycle done

--- notification.py
+++ notification.py
@@ -1,11 +1,7 @@
-import requests
 import time
-import hashlib
-
 import selenium.common
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
 
 url = 'https://edu.21-school.ru'
 CodeReview = 'https://edu.21-school.ru/projects/code-review'
@@ -16,25 +12,26 @@ def WebMonitoring(MyName, MyPass):
         driver = webdriver.Chrome('C:\\chromedriver\chromedriver.exe')
         time.sleep(2)
         driver.get(url)
-        time.sleep(5)
+        time.sleep(3)
         login = driver.find_element(By.NAME, "username")
         password = driver.find_element(By.NAME, "password")
         login.send_keys(MyName)
         password.send_keys(MyPass)
         driver.find_element(By.XPATH, '//*[@id="login"]/div/div/div[2]/div/div/form/div[3]/button').click()
-        time.sleep(5)
+        time.sleep(3)
         driver.get(CodeReview)
-        time.sleep(5)
-        elem = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/div/section/div/section[2]')
-        # цикл для проверки нахождения элемента
+        time.sleep(3)
+        while True:
+            time.sleep(20)
+            driver.refresh()
+            time.sleep(3)
+            elem = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/div/section/div/section[2]')
+            print("NO CODE REVIEW")
 
-        time.sleep(100)
-        driver.quit()
     except selenium.common.NoSuchElementException:
-        WebMonitoring()
+        print('CODE REVIEW!!!')
+        WebMonitoring(MyName, MyPass)
 
 
 if __name__ == "__main__":
     WebMonitoring(MyName, MyPass)
-
-
